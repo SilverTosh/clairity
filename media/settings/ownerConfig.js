1aaa318e59b369f5094b1eb4a0ef2e3c71e8b097
SilverTosh <silvestermigwi76@gmail.com>
2025-05-15 16:31:02 +0300

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -2,9 +2,9 @@
 const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
-global.ownername = 'xZiyy';
-global.owner = '6283804078729';
-global.botname = '6285697725326';
+global.ownername = 'KINGVON';
+global.owner = '254720326316';
+global.botname = 'KINGVON MD';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
